a460eb6f1ec1017c691f11ba4f93732bcdbdfe3f
yugang.cao <yugang.cao@tusen.ai>
2023-10-19 18:53:37 +0800

    feat: type

---
+++ src/type.ts
@@ -0,0 +1,24 @@
+export type Challenge = {
+  /* challenge id */
+  id: string
+  /* challenge title */
+  title: string
+  /* challenge content description */
+  content: string
+  /* challenge difficulty */
+  difficulty: number
+  /* challenge category */
+  category: string
+  /* challenge tags */
+  tags: string[]
+  /* challenge answer */
+  answer: string
+  /* challenge author */
+  author: string
+  /* challenge hit */
+  hit?: string
+  /* challenge initSql */
+  initSql: string
+  /* challenge defaultSql */
+  defaultSql: string
+}
