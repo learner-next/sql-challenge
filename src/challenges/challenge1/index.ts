adea22f22cd3e334d6df0f8b98b745c87e7d9f69
Yugang Cao <1442770751@qq.com>
2023-10-22 21:31:26 +0800

    fix: add challenge 2 (#10)

--- src/challenges/challenge1/index.ts
+++ src/challenges/challenge1/index.ts
@@ -1,30 +1,30 @@
 import type { Challenge } from '@/type'
 import content from './content.md?raw'
 
-const challenge1: Challenge = {
+const challenge: Challenge = {
   id: 'challenge1',
   title: '基础语法 - 创建 - 创建数据表',
   content: content,
   difficulty: 1,
   category: '创建表',
   tags: ['create'],
-  answerSql: `CREATE TABLE student (
-    id integer not null primary key,
-    name varchar(256) not null,
-    age int not null default 20,
-    gender char(4) not null,
-    birthday date null,
-    address varchar(256) null
+  answerSql: `CREATE TABLE IF NOT EXISTS student (
+    id INTEGER NOT NULL PRIMARY KEY,
+    name VARCHAR(256) NOT NULL,
+    age INT  NOT NULL,
+    gender CHAR(4) NOT NULL,
+    birthday DATE NULL,
+    address VARCHAR(256) NULL
   );
-  INSERT INTO student (name, age, gender, birthday, address) values('小明',20,'男','1998-12-20',null),('小丽',18,'女',null,'上海市浦东区');
+  INSERT INTO student (name, age, gender, birthday, address) VALUES('小明',20,'男','1998-12-20',null),('小丽',18,'女',null,'上海市浦东区');
   `,
   answer: 'SELECT * FROM student;',
   showTableSql: 'SELECT * FROM student;',
   author: 'Talljack',
   initSql: '',
-  defaultSql: 'CREATE TABLE student (id integer);',
+  defaultSql: 'CREATE TABLE IF NOT EXISTS student (id integer);',
   type: 'main',
   sqlType: 'create'
 }
 
-export default challenge1
+export default challenge
