38d296adb91e2b71a74facd26ec81f68b7fcda5f
Yugang Cao <1442770751@qq.com>
2023-10-23 21:59:09 +0800

    feat: challenge 4

---
+++ src/challenges/challenge3/initSql.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS `student` (
+    id INTEGER PRIMARY KEY NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    age INT NOT NULL,
+    gender CHAR(4) NOT NULL,
+    birthday DATE NULL,
+    address VARCHAR(255) NULL
+);
+
+INSERT INTO `student` (name, age, gender, birthday, address)
+VALUES ('小明', 20, '男', '1998-12-20', null);
+INSERT INTO `student` (name, age, gender, birthday, address)
+VALUES ('小丽', 18, '女', null, '上海市浦东区');
+INSERT INTO `student` (name, age, gender, birthday, address)
+VALUES ('小王', 28, '男', '1997-08-11', '北京市海淀区');
+INSERT INTO `student` (name, age, gender, birthday, address)
+VALUES ('小李', 36, '男', '1989-02-18', '北京市朝阳区');
--- src/challenges/mainChallenges.ts
+++ src/challenges/mainChallenges.ts
@@ -1,6 +1,7 @@
 import challenge1 from './challenge1'
 import challenge2 from './challenge2'
+import challenge3 from './challenge3'
 
-const mainChallenges = [challenge1, challenge2]
+const mainChallenges = [challenge1, challenge2, challenge3]
 
 export default mainChallenges
