d8bb2468ad554c41bb64414eb69f05e1fdcd67ae
Yugang Cao <1442770751@qq.com>
2023-10-24 21:13:12 +0800

    feat: add challenge4

--- src/challenges/mainChallenges.ts
+++ src/challenges/mainChallenges.ts
@@ -1,7 +1,8 @@
 import challenge1 from './challenge1'
 import challenge2 from './challenge2'
 import challenge3 from './challenge3'
+import challenge4 from './challenge4'
 
-const mainChallenges = [challenge1, challenge2, challenge3]
+const mainChallenges = [challenge1, challenge2, challenge3, challenge4]
 
 export default mainChallenges
