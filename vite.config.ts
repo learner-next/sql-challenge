f807bcbc0fa5eab56d417b964bfaad986d0bef9f
zll600 <3400692417@qq.com>
2023-10-31 23:29:42 +0800

    feat(vite): add code_inspector_plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,17 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import tsconfigPaths from 'vite-tsconfig-paths'
+import { CodeInspectorPlugin } from 'code-inspector-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tsconfigPaths()],
+  plugins: [
+    react(),
+    tsconfigPaths(),
+    CodeInspectorPlugin({
+      bundler: 'vite'
+    })
+  ],
   esbuild: {
     drop: ['console', 'debugger']
   },
