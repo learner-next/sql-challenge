40479a09c11227b0c256ac52ade297d78a6fa288
Yugang Cao <1442770751@qq.com>
2023-10-31 22:08:20 +0800

    feat: index

---
+++ src/challenges/createChallenges/table-index/initSql.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS users (
+    user_id INT,
+    name VARCHAR(256) NOT NULL,
+    card_id INT NOT NULL,
+    address VARCHAR(256) NOT NULL,
+    PRIMARY KEY (user_id)
+);
+
+CREATE INDEX name_index ON users (name);
+
+CREATE UNIQUE INDEX card_id_index ON users (card_id);
