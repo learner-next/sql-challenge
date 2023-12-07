67cf182a82168d7a57ae93e48c27de3b7a11f56c
Yugang Cao <1442770751@qq.com>
2023-12-07 10:07:05 +0800

    feat: monaco editor lib (#87)

--- src/challenges/selectChallenges/select-having/index.ts
+++ src/challenges/selectChallenges/select-having/index.ts
@@ -4,7 +4,7 @@ import type { Challenge } from '@/type'
 
 const challenge: Challenge = {
   id: 'select-having',
-  title: '高级查询 - HAVING',
+  title: ' 高级查询 - HAVING',
   content,
   initSql,
   author: 'Talljack',
@@ -13,11 +13,11 @@ const challenge: Challenge = {
   category: 'select',
   tags: ['select', 'having'],
   answerSql:
-    'SELECT name, class, AVG(score) AS avg_score FROM student GROUP BY class HAVING avg_score > 80 OR avg_score < 20 ORDER BY avg_score DESC;',
-  defaultSql: 'SELECT name, class, score FROM student;',
+    'SELECT name, className, AVG(score) AS avg_score FROM student GROUP BY className HAVING avg_score > 80 OR avg_score < 20 ORDER BY avg_score DESC;',
+  defaultSql: 'SELECT name, className, score FROM student;',
   showTableSql: 'SELECT * FROM student;',
 
-  hit: 'Having 子句用于在 GROUP BY 语句中指定过滤条件。可使用多个聚合函数，如 COUNT()、SUM()、AVG() 等来进行过滤。'
+  hit: 'Having 子句用于在 GROUP BY 语句中指定过滤条件。可使用多个聚合函数，如 COUNT ()、SUM ()、AVG () 等来进行过滤。'
 }
 
 export default challenge
--- src/challenges/selectChallenges/select-having/initSql.sql
+++ src/challenges/selectChallenges/select-having/initSql.sql
@@ -1,67 +1,67 @@
 CREATE TABLE IF NOT EXISTS `student` (
-  `id` INT AUTOINCREMENT,
+  `id` INT AUTOINCREMENT NOT NULL,
   `name` varchar(255) NOT NULL,
-  `class` VARCHAR(20) NOT NULL,
+  `className` VARCHAR(20) NOT NULL,
   `score` INT NOT NULL,
   PRIMARY KEY (`id`)
 );
 
 INSERT INTO `student` (
-  name, class, score
+  name, className, score
 ) VALUES (
   'Alice', 'A', 70
 );
 
 INSERT INTO `student` (
-  name, class, score
+  name, className, score
 ) VALUES (
   'Bob', 'A', 80
 );
 
 INSERT INTO `student` (
-  name, class, score
+  name, className, score
 ) VALUES (
   'Charlie', 'B', 90
 );
 
 INSERT INTO `student` (
-  name, class, score
+  name, className, score
 ) VALUES (
   'David', 'A', 80
 );
 
 INSERT INTO `student` (
-  name, class, score
+  name, className, score
 ) VALUES (
   'Eve', 'B', 78
 );
 
 INSERT INTO `student` (
-  name, class, score
+  name, className, score
 ) VALUES (
   'Frank', 'C', 90
 );
 
 INSERT INTO `student` (
-  name, class, score
+  name, className, score
 ) VALUES (
   'Grace', 'C', 85
 );
 
 INSERT INTO `student` (
-  name, class, score
+  name, className, score
 ) VALUES (
   'Heidi', 'D', 75
 );
 
 INSERT INTO `student` (
-  name, class, score
+  name, className, score
 ) VALUES (
   'Grace', 'E', 18
 );
 
 INSERT INTO `student` (
-  name, class, score
+  name, className, score
 ) VALUES (
   'Heidi', 'E', 20
 );
