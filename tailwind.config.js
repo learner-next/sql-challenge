3cab4ee4d43d0d6932d1a007000948c0c73dd8c4
yugangcao <yugangcao@deeproute.ai>
2024-12-06 19:33:44 +0800

    feat: update deps

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+import animate from 'tailwindcss-animate'
+
+export default {
   darkMode: ['class'],
   content: [
     './index.html',
@@ -73,5 +75,5 @@ module.exports = {
       },
     },
   },
-  plugins: [require('tailwindcss-animate')],
+  plugins: [animate],
 }
